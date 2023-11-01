95cbb93d431bd5148eab7a644f0011e5081ba215
kxhcfk <reveersed@mail.ru>
2023-11-01 16:13:20 +0300

    feat: simple app

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Likes count: <code>{{ likes }}</code></h1>
+    <h1>Dislikes count: <code>{{ dislikes }}</code></h1>
+    <button @click="addLike">like</button>
+    <button @click="addDislike">dislike</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0,
+    }
+  },
+  methods: {
+    addLike() {
+      this.likes += 1;
+    },
+    addDislike() {
+      this.dislikes += 1;
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
