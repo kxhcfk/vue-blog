3c8732c0d6e3e10c2b4aa232edbfef8da9e35ef7
kxhcfk <reveersed@mail.ru>
2023-11-01 18:42:32 +0300

    feat: remove list item

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,11 +1,14 @@
 <template>
-  <div>
-    <h3>User list</h3>
+  <div v-if="posts.length > 0">
+    <h3>Posts list</h3>
     <post-item
         v-for="post in posts"
         :post="post"
+        :key="post.id"
+        @remove="$emit('remove', post)"
     />
   </div>
+  <h2 v-else>Posts list is empty</h2>
 </template>
 
 <script>
