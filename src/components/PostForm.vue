d0870aa2b9757d861d937536d2a2fa92efb0fdcc
kxhcfk <reveersed@mail.ru>
2023-11-01 18:34:32 +0300

    feat: ui input component

--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -1,13 +1,14 @@
 <template>
   <form @submit.prevent>
     <h3>create post</h3>
-    <input v-model="post.title" class="input" type="text" placeholder="title">
-    <input v-model="post.body" class="input" type="text" placeholder="body">
-    <button class="btn" @click="createPost">create</button>
+    <my-input v-model="post.title" type="text" placeholder="title"/>
+    <my-input v-model="post.body" type="text" placeholder="body"/>
+    <my-button @click="createPost">create</my-button>
   </form>
 </template>
 
 <script>
+
 export default {
   data() {
     return {
@@ -37,23 +38,5 @@ form {
   display: flex;
   flex-direction: column;
   gap: 8px;
-}
-
-.input {
-  display: block;
-  width: 100%;
-  box-shadow: inset 0 0 0 1px #5b4ae1;
-  padding: 10px 15px;
-  border: 0;
-}
-
-.btn {
-  border: 0;
-  background: none;
-  align-self: flex-end;
-  margin-top: 15px;
-  padding: 10px 15px;
-  color: #5b4ae1;
-  box-shadow: inset 0 0 0 1px #5b4ae1;
 }
 </style>
---
+++ src/components/UI/MyInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <input :value="modelValue" @input="updateInput" class="input" type="text">
+</template>
+
+<script>
+export default {
+  name: 'my-input',
+  props: {
+    modelValue: [String, Number],
+  },
+  methods: {
+    updateInput(e) {
+      this.$emit('update:modelValue', e.target.value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.input {
+  display: block;
+  width: 100%;
+  box-shadow: inset 0 0 0 1px #5b4ae1;
+  padding: 10px 15px;
+  border: 0;
+}
+</style>
